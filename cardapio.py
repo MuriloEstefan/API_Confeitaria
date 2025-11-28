337064ff46173ba97336b5b3ae15a0f5dda5bfeb
muriloestefano19 <muriloestefano19@gmail.com>
2025-11-28 22:59:23 +0000

    start project

---
+++ cardapio.py
@@ -0,0 +1,16 @@
+def get_cardapio():
+    cardapio = [
+        {
+            'id': 1,
+            'titulo': 'Bolo de Chocolate',
+            'descricao': 'O bolo é delicioso igual sua mamae',
+            'foto': ''
+        },
+        {
+            'id': 2,
+            'titulo': 'Bolo de morango',
+            'descricao': 'Delicioso bolo de morango, vou mostrar o morangao p c',
+            'foto': ''
+        }
+    ]
+    return cardapio
